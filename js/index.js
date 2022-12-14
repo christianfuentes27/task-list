245e883fb7dea0ee74994c17a6919f00d0221de5
Christian Fuentes <cjfuentes27@ieszaidinvergeles.org>
2022-12-14 14:21:12 +0100

    Add task without drag

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-let desktop = $('.desktop');
+var count = 1;
+
+$(function () {
+    $('.item').draggable();
+});
+
+$('.addTask').on('click', (e) => {
+    let item = `<div class="item">${$('#taskContent').val()}</div>`;
+    $('.desktop').append(item);
+    console.log(item);
+    // task.html($('#taskContent').val());
+    // $('.item').html($('#taskContent').val());
+    count++;
+});
