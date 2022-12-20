350bbba408ffe8bd53bf708a498fb39511b9a106
Christian <cjfuentes27@ieszaidinvergeles.org>
2022-12-20 21:31:47 +0100

    Task manager

--- js/index.js
+++ js/index.js
@@ -1,148 +1,93 @@
-// var count = 1;
+$(function () {
+  // There's the gallery and the trash
+  // init();
 
-// $(function () {
-//     $('.item').draggable();
-// });
+  var $gallery = $("#gallery"),
+    $todolist = $("#todolist"),
+    list = [];
 
-// $('.addTask').on('click', (e) => {
-//     let dataPost = `post-${count}`;
-//     let item = `<div class="item" data-post="${dataPost}">${$('#taskContent').val()}</div>`;
-//     $('.taskList').append(item);
-//     $(`div[data-post='${dataPost}']`).draggable();
-//     count++;
-// });
-
-$( function() {
- 
-    // There's the gallery and the trash
-    var $gallery = $( "#gallery" ),
-      $trash = $( "#trash" );
-      $todo = $('#todo');
- 
-    // Let the gallery items be draggable
-    $( "li", $gallery ).draggable({
+  $('.add-btn').on('click', (e) => {
+    $(`<li class="ui-widget-content ui-corner-tr"><p>${$('.task-text').val()}</p></li>`).draggable({
       cancel: "a.ui-icon", // clicking an icon won't initiate dragging
       revert: "invalid", // when not dropped, the item will revert back to its initial position
       containment: "document",
       helper: "clone",
       cursor: "move"
-    });
- 
-    // Let the trash be droppable, accepting the gallery items
-    $trash.droppable({
-      accept: "#gallery > li",
-      classes: {
-        "ui-droppable-active": "ui-state-highlight"
-      },
-      drop: function( event, ui ) {
-        deleteImage( ui.draggable );
-      }
-    });
-    
-    
-    $trash.droppable({
-      accept: "#gallery > li",
-      classes: {
-        "ui-droppable-active": "ui-state-highlight"
-      },
-      drop: function( event, ui ) {
-        deleteImage( ui.draggable );
-      }
-    });
-    
-    $trash.droppable({
-      accept: "#gallery > li",
-      classes: {
-        "ui-droppable-active": "ui-state-highlight"
-      },
-      drop: function( event, ui ) {
-        deleteImage( ui.draggable );
-      }
-    });
-    
+    }).appendTo($('#gallery'));
+    $('.task-text').val('');
+  });
 
+  // Let the gallery items be draggable
+  $("li", $gallery).draggable({
+    cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+    revert: "invalid", // when not dropped, the item will revert back to its initial position
+    containment: "document",
+    helper: "clone",
+    cursor: "move"
+  });
 
-    // Let the gallery be droppable as well, accepting items from the trash
-    $gallery.droppable({
-      accept: "#trash li",
-      classes: {
-        "ui-droppable-active": "custom-state-active"
-      },
-      drop: function( event, ui ) {
-        recycleImage( ui.draggable );
-      }
+  // Let the trash be droppable, accepting the gallery items
+  $todolist.droppable({
+    accept: "#gallery > li",
+    classes: {
+      "ui-droppable-active": "ui-state-highlight"
+    },
+    drop: function (event, ui) {
+      deleteTask(ui.draggable);
+    }
+  });
+
+  // Let the gallery be droppable as well, accepting items from the trash
+  $gallery.droppable({
+    accept: "#todolist li",
+    classes: {
+      "ui-droppable-active": "custom-state-active"
+    },
+    drop: function (event, ui) {
+      recycleTask(ui.draggable);
+    }
+  });
+
+  // Image deletion function
+  function deleteTask($item) {
+    $item.fadeOut(function () {
+      var $list = $("ul", $todolist).length ?
+        $("ul", $todolist) :
+        $("<ul class='gallery ui-helper-reset'/>").appendTo($todolist);
+
+      $item.appendTo($list).fadeIn();
     });
- 
-    // Image deletion function
-    var recycle_icon = "<a href='link/to/recycle/script/when/we/have/js/off' title='Recycle this image' class='ui-icon ui-icon-refresh'>Recycle image</a>";
-    function deleteImage( $item ) {
-      $item.fadeOut(function() {
-        var $list = $( "ul", $trash ).length ?
-          $( "ul", $trash ) :
-          $( "<ul class='gallery ui-helper-reset'/>" ).appendTo( $trash );
- 
-        $item.find( "a.ui-icon-trash" ).remove();
-        $item.append( recycle_icon ).appendTo( $list ).fadeIn(function() {
-          $item
-            .animate({ width: "48px" })
-            .find( "img" )
-              .animate({ height: "36px" });
-        });
-      });
-    }
- 
-    // Image recycle function
-    var trash_icon = "<a href='link/to/trash/script/when/we/have/js/off' title='Delete this image' class='ui-icon ui-icon-trash'>Delete image</a>";
-    function recycleImage( $item ) {
-      $item.fadeOut(function() {
-        $item
-          .find( "a.ui-icon-refresh" )
-            .remove()
-          .end()
-          .css( "width", "96px")
-          .append( trash_icon )
-          .find( "img" )
-            .css( "height", "72px" )
-          .end()
-          .appendTo( $gallery )
-          .fadeIn();
-      });
-    }
- 
-    // Image preview function, demonstrating the ui.dialog used as a modal window
-    function viewLargerImage( $link ) {
-      var src = $link.attr( "href" ),
-        title = $link.siblings( "img" ).attr( "alt" ),
-        $modal = $( "img[src$='" + src + "']" );
- 
-      if ( $modal.length ) {
-        $modal.dialog( "open" );
-      } else {
-        var img = $( "<img alt='" + title + "' width='384' height='288' style='display: none; padding: 8px;' />" )
-          .attr( "src", src ).appendTo( "body" );
-        setTimeout(function() {
-          img.dialog({
-            title: title,
-            width: 400,
-            modal: true
-          });
-        }, 1 );
-      }
-    }
- 
-    // Resolve the icons behavior with event delegation
-    $( "ul.gallery > li" ).on( "click", function( event ) {
-      var $item = $( this ),
-        $target = $( event.target );
- 
-      if ( $target.is( "a.ui-icon-trash" ) ) {
-        deleteImage( $item );
-      } else if ( $target.is( "a.ui-icon-zoomin" ) ) {
-        viewLargerImage( $target );
-      } else if ( $target.is( "a.ui-icon-refresh" ) ) {
-        recycleImage( $item );
-      }
- 
-      return false;
+    list.push($item);
+  }
+
+  // Image recycle function
+  function recycleTask($item) {
+    $item.fadeOut(function () {
+      $item
+        .appendTo($gallery)
+        .fadeIn();
     });
-  } );
+    list.splice(list.indexOf($item), 1);
+    sendMessage($item);
+  }
+
+  function sendMessage($item) {
+    const msg = {
+      type: 'task',
+      content: $item[0].children[0].innerHTML,
+      list
+    }
+    // socket.send(JSON.stringify(msg));
+  }
+
+  function init() {
+    socket = new WebSocket(`ws://localhost:8023`);
+    socket.onopen = (e) => {
+      state = true;
+    };
+    socket.onmessage = (e) => {
+      let data = JSON.parse(e.data);
+      //Take array and display all tasks
+    }
+  }
+});
